66aaa4eece491ad677e15b04ad2cb65fe4dfc2bf
TaheerAhmed <85991537+TaheerAhmed@users.noreply.github.com>
2024-06-24 15:25:40 +0530

    tried changing approach for reducing complexity

--- check_limits.py
+++ check_limits.py
@@ -1,13 +1,32 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
+def temp_ok(tmp):
   if temperature < 0 or temperature > 45:
     return False
-  elif soc < 20 or soc > 80:
+  else:
+    return True
+def soc_ok(soc):
+  if soc < 20 or soc > 80:
     return False
-  elif charge_rate > 0.8:
+  else:
+    return True
+def charge_ok(charge_rate):
+  if charge_rate > 0.8:
     return False
+  else:
+    return True
 
-  return True
+
+def battery_is_ok(temperature, soc, charge_rate):
+  # if temperature < 0 or temperature > 45:
+  #   return False
+  # elif soc < 20 or soc > 80:
+  #   return False
+  # elif charge_rate > 0.8:
+  #   return False
+  if(temp_ok(temperature)and charge_ok(charge_rate) and soc_ok(soc)):
+    return True
+  else:
+    return True
 
 
 if __name__ == '__main__':
